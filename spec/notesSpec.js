8d5d57864060c7d3066b9b98f5230a4ff9c7f47b
Cynthia Anyaeriuba <cynthia.a@Cynthias-MacBook-Pro.local>
2021-05-11 15:07:33 +0100

    built test logic

--- spec/notesSpec.js
+++ spec/notesSpec.js
@@ -1,13 +1,19 @@
-function testNoteIsCreatedWithText() {
-  let note = new Note();
+// function testNoteIsCreatedWithText() {
+//   let note = new Note();
 
-  if(note.text === undefined) {
-    console.log("FAIL");
-  } else {
-    console.log("PASS");
-  }
-};
+//   if(note.text === undefined) {
+//     console.log("FAIL");
+//   } else {
+//     console.log("PASS");
+//   }
+// };
 
-testNoteIsCreatedWithText();
+// testNoteIsCreatedWithText();
 
+it('2 + 2 is equal to 4 ', function() {
+  expect(2+2).toEqual(4)
+});
 
+it('2 + 3 is equal to 4 ', function() {
+  expect(2+3).toEqual(4)
+});
--- spec/testLibrary.js
+++ spec/testLibrary.js
@@ -1,1 +1,20 @@
 // helper methods and matchers for testing framework
+"use strict";
+
+function expect(a) {
+  return {
+    toEqual: function(b) {
+      if (a == b) {
+        console.log("Pass");
+      } else {
+        console.log("Fail");
+      };
+    }
+  };
+};
+
+function it(label, callback) {
+  console.log(`Test: ${label}`)
+  callback()
+};
+
