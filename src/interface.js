ef33c3606ed129de9889fcae875e01b4d13b517f
Kerri McMahon <kerrimcm25@gmail.com>
2021-05-12 17:20:33 +0100

    Appended abbrevaited notes as links

--- src/interface.js
+++ src/interface.js
@@ -2,30 +2,32 @@
 
 document.addEventListener("DOMContentLoaded", function() {
 
+  let submitText = document.querySelector('#add-note');
 
+  submitText.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let inputText = document.querySelector('#note-text').value;
+    const newNote = createNote(inputText);
+    appendToList(newNote);
+  })
 
-let submitText = document.querySelector('#add-note');
+  function createNote(inputText) {
+    let note = new Note(inputText);
+    return note;
+  }
 
-submitText.addEventListener('submit', (event) => {
-  event.preventDefault();
-  let inputText = document.querySelector('#note-text').value;
-  let newNote = createNote(inputText);
-  appendToList(newNote);
-})
+  function appendToList(newNote) {
+    // create element <li>note object</li> + id= n++ 
+    let shortNote = newNote.twentyChars;
+    let listItem = document.createElement("LI");
+    let noteText = document.createTextNode(shortNote);
+    let link = document.createElement('a');
+    
+    link.textContent = shortNote;
+    link.href = '#';
+    link.id = shortNote;
 
-function createNote(inputText) {
-  let note = new Note(inputText);
-  return note;
-}
-
-function appendToList(newNote) {
-  // create element <li>note object</li> + id= n++ 
-  let shortNote = newNote.twentyChars;
-  let listItem = document.createElement("LI");
-  let noteText = document.createTextNode(shortNote);
-  listItem.appendChild(noteText);
-  document.getElementById("note-list").appendChild(listItem);
-}
-
+    listItem.appendChild(link);
+    document.getElementById("note-list").appendChild(listItem); 
+  }
 });
-
