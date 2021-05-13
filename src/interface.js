2ba88c7bdc7ebc833b28344893eaa487a5b60d3e
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-05-13 17:26:11 +0100

    Emoji API can be printed to page but it isnt a note object, so cannot call '.twentyChars' method on it

--- src/interface.js
+++ src/interface.js
@@ -8,7 +8,9 @@ document.addEventListener("DOMContentLoaded", function() {
     event.preventDefault();
     let inputText = document.querySelector('#note-text').value;
     const newNote = createNote(inputText);
-    appendToList(newNote);
+    console.log(newNote);
+    //appendToList(newNote);
+    getPostData(newNote.getNote());
   })
 
   function createNote(inputText) {
@@ -17,16 +19,33 @@ document.addEventListener("DOMContentLoaded", function() {
   }
 
   function appendToList(newNote) {
-    let shortNote = newNote.twentyChars;
+    //let shortNote = newNote.twentyChars;
     let listItem = document.createElement("LI");
     let link = document.createElement('a');
     
-    link.textContent = shortNote;
+    link.textContent = newNote;
     link.href = '#';
-    link.id = shortNote;
+    link.id = newNote;
 
     listItem.appendChild(link);
     document.getElementById("note-list").appendChild(listItem); 
+  }
+
+  function getPostData(inputText) {
+    fetch("https://makers-emojify.herokuapp.com/", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({"text": inputText}),
+    })
+    .then((response) => response.json())
+    .then(data => {
+      appendToList(data.emojified_text);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
   }
 });
 
