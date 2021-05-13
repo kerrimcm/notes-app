8276fc47bae9814e6946311a489ac909a8015f46
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-05-13 17:58:16 +0100

    Added console logs 1-6 to interface to determine callback order (current order: 1, 6, 2, 5, 3, 4) - (commented out array methods)

--- src/interface.js
+++ src/interface.js
@@ -3,14 +3,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
   let submitText = document.querySelector('#add-note');
+  // const noteList = [];
+  console.log(1)
 
   submitText.addEventListener('submit', (event) => {
     event.preventDefault();
     let inputText = document.querySelector('#note-text').value;
     const newNote = createNote(inputText);
     console.log(newNote);
+    console.log(2)
     //appendToList(newNote);
-    getPostData(newNote.getNote());
+    getEmojiData(newNote.getNote());
+    console.log(5)
   })
 
   function createNote(inputText) {
@@ -29,9 +33,10 @@ document.addEventListener("DOMContentLoaded", function() {
 
     listItem.appendChild(link);
     document.getElementById("note-list").appendChild(listItem); 
+    console.log(3)
   }
 
-  function getPostData(inputText) {
+  function getEmojiData(inputText) {
     fetch("https://makers-emojify.herokuapp.com/", {
       method: 'POST',
       headers: {
@@ -42,12 +47,21 @@ document.addEventListener("DOMContentLoaded", function() {
     .then((response) => response.json())
     .then(data => {
       appendToList(data.emojified_text);
+      console.log(4)
     })
     .catch((error) => {
       console.error('Error:', error);
     });
   }
+  console.log(6)
 });
+
+// function storeNoteToArray(newNote) {
+//   noteList.push(newNote);
+// };
+
+// function displayFullnote() {
+
 
 // create element <li>note object</li> + id= n++ 
 // let noteText = document.createTextNode(shortNote); //not currently in use
