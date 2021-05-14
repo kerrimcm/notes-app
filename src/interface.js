9c50fd1f737ea8ea596bf1b68a579088914ee8f5
Cynthia Anyaeriuba <canyaeriuba@gmail.com>
2021-05-14 21:03:09 +0100

    refactored interface removing console.logs

--- src/interface.js
+++ src/interface.js
@@ -6,9 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
   let idCount = 0
   let listOfNotes = document.querySelector('#note-list');
   const noteArray = [];
-
-  console.log(noteArray);
-
+  
   submitText.addEventListener('submit', (event) => {
     event.preventDefault();
     let inputText = document.querySelector('#note-text').value;
@@ -55,47 +53,34 @@ document.addEventListener("DOMContentLoaded", function() {
   }
   
   listOfNotes.addEventListener('click', (event) => {
-    console.log(1)
-    console.log(event.target.id)
-    
     let clickedNoteId = event.target.id
     makeUrlChangeShowFullNote(clickedNoteId);
-    console.log(2)
   });
 
   function makeUrlChangeShowFullNote(clickedNoteId) {
-    console.log(3)
     document.addEventListener("hashchange", showNoteForCurrentPage(clickedNoteId));
-    console.log(4)
   };
 
   function showNoteForCurrentPage(clickedNoteId) {
-    console.log(5)
     showNote(clickedNoteId);
-    console.log(6)
   };
 
 
   function showNote(id) {
-    console.log(8)
-    console.log(id)
+ 
     let newBody = document.getElementById("page")
     let space = document.createElement("BR");
     let closeButton = document.createElement("a");
+
     closeButton.innerHTML = "Close";
     closeButton.id = "close-button";
     closeButton.href = "";
+
     newBody.innerHTML = noteArray[id].text;
     newBody.appendChild(space);
     newBody.appendChild(closeButton);
-    console.log(9)
+   
   };
 
-  // document.addEventListener('click', (event) => {
-  //   if(event.target.id == "close-button"){
-  //   let currentPage = document.getElementById("page")
-  //   currentPage.innerHTML = homePage
-  //   }
-  // });
 });
 
